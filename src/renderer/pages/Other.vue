4b260dbeb73fc3745786b3a10c4704b554753748
wang-boss <1002726239@qq.com>
2021-01-28 08:57:57 +0800

    Add Other page

---
+++ src/renderer/pages/Other.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: Letmeouted
+ * @Email: 1002726239@qq.com
+ * @FilePath: \AneuFilerVue\src\renderer\pages\other.vue
+-->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
